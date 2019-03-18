82bb5fe41984bf0e7bac65c82f35d09f7182506c
John Gallaugher <john.gallaugher@bc.edu>
2019-03-18 17:16:25 -0400

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by John Gallaugher on 3/18/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
